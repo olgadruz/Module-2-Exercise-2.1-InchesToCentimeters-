3b71eb8d8b0ce97bd59cf08a2ff7266be7c07550
Оля Дружкова <Druzhkova@MacBook-Pro-Ola.local>
2024-09-13 14:51:19 -0400

    first draft

---
+++ InchesToCentimeters.cs
@@ -0,0 +1,11 @@
+﻿namespace InchesToCentimeters;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double inch = 2.54;
+        double cm = 15994;
+        Console.WriteLine(cm * inch + " centimeters");
+    }
+}
